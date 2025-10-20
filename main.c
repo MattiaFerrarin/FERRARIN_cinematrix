67f2b95d27a9390c3042ac24576209d76837fbc1
12999533 <12999533@LABMOBILE12>
2025-10-20 13:25:47 +0200

    Start

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    char *Title;
+    char *Director;
+    int ReleaseYear; // int because it's easier than char * even if it's inefficient
+    float Length;
+}Film;
+
+Film *LoadDatabase();
+void AddFilm(Film *film);
+Film *SearchFilmsInDatabase(char *string, Film *database);
+Film *OrderDatabase(Film *database);
+// save and others
 
 int main(void) {
-    printf("Hello, World!\n");
+    Film *database = (Film *)malloc(sizeof(Film));
+
+
     return 0;
 }
